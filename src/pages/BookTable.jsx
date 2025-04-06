ae2acd355be4c446ef03f580a28e4ec65846d51a
OLAYEMI <awoyemiolayemi27@gmail.com>
2025-04-06 20:04:56 +0100

    made changes

--- src/pages/BookTable.jsx
+++ src/pages/BookTable.jsx
@@ -8,7 +8,7 @@ const BookTable = () => {
   return (
     <div className="bg-gray-950 pb-14">
       <div className="relative">
-        <div
+        <div 
           className="absolute top-0 left-0 w-full opacity-20 h-[60vh] lg:h-[90vh] bg-fixed bg-no-repeat bg-cover"
           style={{ backgroundImage: `url('https://html.awaikenthemes.com/spicyhunt/images/page-header-bg.jpg')` }}
         ></div>
