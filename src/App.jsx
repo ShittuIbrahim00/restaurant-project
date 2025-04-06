8c4474f681968dbe2b79b1a4cb7bf3ce68fc6953
OLAYEMI <awoyemiolayemi27@gmail.com>
2025-04-06 19:49:04 +0100

    merge olayemibranch to main

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import CustomCursor from "./components/CustomCursor";
 import Home from "./pages/Home";
-<<<<<<< HEAD
-=======
+import OurChef from "./pages/OurChef";
+import ChefDetail from "./pages/ChefDetail";
+import Footer from "./footer/Footer";
 import OurChef from "./pages/OurChef";
 import Footer from "./footer/Footer";
 import ChefDetail from "./pages/ChefDetail";
->>>>>>> faa0656543913b2722bc969c56de73042fbb6e2a
+
 
 function App() {
   const [loading, setIsLoading] = useState(true);
@@ -18,30 +19,30 @@ function App() {
   }, []);
 
   return (
-<<<<<<< HEAD
+
     <div>
       {!loading && <Navbar />}
-=======
-    <div className="min-h-full">
-      <div className="">
-        <Navbar />
-      </div>
->>>>>>> faa0656543913b2722bc969c56de73042fbb6e2a
-      <CustomCursor />
-      
-      {/* Defining Routes for different pages */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/chefs" element={<OurChef />} />
-        <Route path="/chefs/:id" element={<ChefDetail/>}/>
-        {/* You can add a default route for 404-like behavior */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-      <div className="">
-        <Footer />
-      </div>
-    </div>
-  );
-}
 
+      <div className="min-h-full">
+        <div className="">
+          <Navbar />
+        </div>
+        <CustomCursor />
+
+        {/* Defining Routes for different pages */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/chefs" element={<OurChef />} />
+          <Route path="/chefs/:id" element={<ChefDetail />} />
+          {/* You can add a default route for 404-like behavior */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+        <div className="">
+          <Footer />
+        </div>
+      </div>
+
+    </div>
+  )
+}
 export default App;
