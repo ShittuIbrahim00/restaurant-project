f5510928af604f417cfb8b2c611a86b9f9bb6029
Shittu Ibrahim <2bitspanky2000@gmail.com>
2025-04-09 15:28:03 +0100

    Adjusted the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,11 @@ import OurChef from "./pages/OurChef";
 import ChefDetail from "./pages/ChefDetail";
 import Footer from "./footer/Footer";
 import BookTable from "./pages/BookTable";
-// import { Elements } from "@stripe/react-stripe-js";
-// import { loadStripe } from "@stripe/stripe-js";
-// import CheckoutForm from "./pages/CheckoutForm";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import CheckoutForm from "./pages/CheckoutForm";
 
-// const stripePromise = loadStripe("pk_test_YOUR_PUBLIC_KEY_HERE");
-
+const stripePromise = loadStripe("pk_test_YOUR_PUBLIC_KEY_HERE");
 
 function App() {
   const [loading, setIsLoading] = useState(true);
@@ -28,40 +27,29 @@ function App() {
   return (
     <div className="font-merienda">
       {!loading && <Navbar />}
-
-      <div className="">
-        <div className="">
-          <Navbar />
-        </div>
-
-        <CustomCursor />
-
-        {loading ? (
-          <Loader />
-        ) : (
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
-            {/* <Route
-              path="/cart-checkout"
-              element={
-                <Elements stripe={stripePromise}>
-                  <CheckoutForm />
-                </Elements>
-              }
-            /> */}
-            <Route path="/chefs" element={<OurChef />} />
-            <Route path="/chefs/:id" element={<ChefDetail />} />
-            <Route path="/reservation" element={<BookTable />} />
-            <Route path="*" element={<Error404 />} />
-          </Routes>
-        )}
-        <div className="">
-          {!loading && <Footer />}
-        </div>
-      </div>
+      <CustomCursor />
+      {loading ? (
+        <Loader />
+      ) : (
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route
+            path="/cart-checkout"
+            element={
+              <Elements stripe={stripePromise}>
+                <CheckoutForm />
+              </Elements>
+            }
+          />
+          <Route path="/chefs" element={<OurChef />} />
+          <Route path="/chefs/:id" element={<ChefDetail />} />
+          <Route path="/reservation" element={<BookTable />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      )}
+      {!loading && <Footer />}
     </div>
-
   );
 }
 
