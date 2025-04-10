0def58b51041cb5f1d94897918c8445e9decd532
unknown <ahmad.mjabdullah@gmail.com>
2025-04-10 11:22:21 +0100

    adjusting menu detail text to be responsive

--- src/pages/MenuItemDetail.jsx
+++ src/pages/MenuItemDetail.jsx
@@ -12,21 +12,21 @@ const MenuItemDetail = () => {
                 />
             </div>
             <div className='absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-70'>
-                <h2 className='text-7xl text-white mb-44'>OUR MENU</h2>
+                <h2 className='absolute top-2/4 text-2xl md:text-4xl lg:text-7xl text-white mb-44'>OUR MENU</h2>
             </div>
         </div>
 
 
         <div className='bg-[#0E110D]'>
             <div className='flex flex-wrap gap-8 mx-5 md:grid md:grid-cols-2 '>
-                <div className='my-20'>
+                <div className='md:my-20'>
                     <img
                         src="https://html.awaikenthemes.com/spicyhunt/images/pizza-img.png"
                         alt="Pizza"
                         className='object-cover opacity-85 rounded-2xl w-[90%] h-[90%] md:[w-60%] md:[h-60%]'
                     />
                 </div>
-                <div className='my-20'>
+                <div className='md:my-20'>
                     <h5 className='font-bold text-[#A6A182]'>CHEF</h5>
                     <h1 className='text-white font-bold text-4xl py-3'>ABOUT THE <span className='text-[#A6A182]'>DISH</span></h1>
                     <p className='text-[#AEB0B4] text-lg'>Sophia Martinez is a talented and passionate Chef de Cuisine, known for her innovative approach to<br/>modern cuisine. With years of experience in fine dining, she leads the kitchen with creativity and<br/>precision. Her expertise in flavor pairing and culinary techniques ensures that every dish not only<br/>meets but exceeds the expectations of the restaurant's guests. William's leadership fosters a<br/>collaborative and efficient kitchen environment, allowing her team to deliver consistently exceptional dining experiences.</p>
